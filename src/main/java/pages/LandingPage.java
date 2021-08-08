c66dc9a410c0904ed852fa673a5d124098adbaa3
qanaha <malvanatalya@ukr.net>
2021-08-09 02:48:20 +0300

    LandPage, search and join course test

--- src/main/java/pages/LandingPage.java
+++ src/main/java/pages/LandingPage.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LandingPage extends ParentPage {
@@ -48,6 +53,21 @@ public class LandingPage extends ParentPage {
 
     @FindBy(xpath = ".//form[@id='login_form']//li[text()='E-mail адрес и/или пароль не верны.']")
     private WebElement invalidCredErrorMessage;
+
+    @FindBy(xpath = ".//input[@class='search-form__input ']")
+    private WebElement inputSearchForm;
+
+    @FindBy(xpath = ".//button[@class='button_with-loader search-form__submit']")
+    private WebElement buttonSearch;
+
+    @FindBy(xpath = ".//div[@data-list-type='search-results']//a[@class='course-card__title']")
+    private List<WebElement> listOfCoursesInSearchResult;
+
+    @FindBy(xpath = ".//aside[@class='course-promo__main-aside']//button[text()='Поступить на курс']")
+    private WebElement buttonJoinTheCourse;
+
+    @FindBy(xpath = ".//div[@class='auth-widget sign-form sign-form__shaking ember-view']")
+    private WebElement registerModalWindow;
 
     public LandingPage(WebDriver webDriver) {
         super(webDriver);
@@ -136,6 +156,11 @@ public class LandingPage extends ParentPage {
         clickOnButtonRegister();
     }
 
+    public LandingPage checkIsRegisterButtonPresent() {
+        Assert.assertTrue(isElementPresent(buttonRegister));
+        return this;
+    }
+
     public HomePage loginWithValidCred() {
         fillLoginFormAndSubmit(TestData.getValidEmail(), TestData.getValidPassword());
         return new HomePage(webDriver);
@@ -149,6 +174,35 @@ public class LandingPage extends ParentPage {
 
     public LandingPage checkValidationEmptyPasswordErrorMessage() {
         Assert.assertEquals("Заполните это поле.", webDriver.findElement(By.name("password")).getAttribute("validationMessage"));
+        return this;
+    }
+
+    public void enterTheCourseName(String courseNameToSearch){
+        enterTextToElement(inputSearchForm,courseNameToSearch);
+    }
+
+    public void clickOnSearchButton(){
+        clickOnElement(buttonSearch);
+    }
+
+    public void clickOnButtonJoinTheCourse(){
+        clickOnElement(buttonJoinTheCourse);
+    }
+
+
+    public  LandingPage searchAndJoinUniqueExistentFreeCourseByUnauthorisedUser(String specificCourseTitle){
+        openLandingPage();
+        enterTheCourseName(specificCourseTitle);
+        clickOnSearchButton();
+        for (WebElement courseTitleLink:listOfCoursesInSearchResult) {
+            if(courseTitleLink.getText().equals(specificCourseTitle)){
+                clickOnElement(courseTitleLink);
+            } else {
+                logger.error("The course not found");
+            }
+        }
+        clickOnButtonJoinTheCourse();
+        new WebDriverWait(webDriver, 10).until(ExpectedConditions.visibilityOf(registerModalWindow));
         return this;
     }
 }
